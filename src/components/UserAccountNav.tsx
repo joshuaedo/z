d3106c50b029f62adaa20abf220fa2eeee4fd9e5
Joshua Edo <joshua.edo01@gmail.com>
2023-08-17 19:06:33 +0000

    add drop down

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,6 +1,6 @@
 import { User } from 'next-auth';
 import React, { FC } from 'react';
-import { DropdownMenu, DropdownMenuTrigger } from './ui/DropDownMenu';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from './ui/DropDownMenu';
 import UserAvatar from './UserAvatar';
 
 interface UserAccountNavProps {
@@ -12,12 +12,21 @@ const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
         <DropdownMenu>
             <DropdownMenuTrigger>
                 <UserAvatar
-                className='h-8 w-8'
-                user={{
-                    name: user.name || null,
-                    image: user.image || null,
-                }} />
+                    className='h-8 w-8'
+                    user={{
+                        name: user.name || null,
+                        image: user.image || null,
+                    }} />
             </DropdownMenuTrigger>
+
+            <DropdownMenuContent className='bg-white' align='end'>
+                <div className='flex items-center justify-start gap-2 p-2'>
+                    <div className='flex flex-col space-y-1 leading-none'>
+                        {user.name && <p className='font-medium'>{user.name}</p>}
+                        {user.email && <p className='w-[200px] truncate text-sm text-zinc-700'>{user.email}</p>}
+                    </div>
+                </div>
+            </DropdownMenuContent>
         </DropdownMenu>
     );
 };
