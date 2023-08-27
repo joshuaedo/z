5039573825759609c0bed6d19f347be46337a3ff
Joshua Edo <joshua.edo01@gmail.com>
2023-08-27 23:01:46 +0000

    Update Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,7 +10,7 @@ const Modal: FC<ModalProps> = ({ modalContainer, children }) => {
   return (
     <div className={`fixed inset-0 bg-zinc-900/20 z-10`}>
       <div
-        className={`container flex items-center h-full ${modalContainer} mx-auto`}
+        className={`container flex items-center h-full ${modalContainer} justify-center`}
       >
         <div className={`relative bg-white h-fit py-20 px-2 rounded-lg`}>
           <div className={`absolute top-4 right-4`}>
