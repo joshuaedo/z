fc87e557c07dbbd2d3091cf1ee4449704e5a0d69
Joshua Edo <joshua.edo01@gmail.com>
2023-08-28 02:13:07 +0000

    Update page.tsx

--- src/app/z/[slug]/submit/page.tsx
+++ src/app/z/[slug]/submit/page.tsx
@@ -24,12 +24,12 @@ if(!community) return notFound()
         <Modal modalContainer='max-w-4xl' showMobileModal={false}>
         <div className="flex flex-col items-start gap-4 md:gap-6">
            <div className="pb-5">
-            <div className="flex justify-between">
+            <div className="flex justify-between w-full">
                <div className="border-b border-gray-200 -ml-2 -mt-2 flex flex-wrap items-baseline">
                   <h3 className="ml-2 mt-2 text-base font-semibold leading-6 text-gray-900">Create Post</h3>
                   <p className="ml-2 mt-1 truncate text-sm text-gray-500s">in z/{params.slug}</p>
                </div>
-              <Button type="submit" className="w-full" form="community-post-form">Post</Button>
+              <Button type="submit" className="" form="community-post-form">Post</Button>
          </div> 
          </div> 
 
