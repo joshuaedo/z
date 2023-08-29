6ed65078660e66c757ccaa21f22e5c7f580e0c45
Joshua Edo <joshua.edo01@gmail.com>
2023-08-29 13:14:18 +0000

    Update Post.tsx

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,9 +1,7 @@
-"use client";
 import Editor from "@/components/Editor";
 import Modal from "@/components/Modal";
 import { Button } from "@/components/ui/Button";
-import { Minimize2 } from "lucide-react";
-import { Maximize2 } from "lucide-react";
+import { Minimize2, Maximize2 } from "lucide-react";
 import { useState } from "react";
 
 interface SubmitPostProps {
@@ -16,13 +14,13 @@ interface SubmitPostProps {
 }
 
 const SubmitPost = ({ params, community }: SubmitPostProps) => {
-  const [showModal, setShowModal] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(true);
 
   const toggleModal = () => {
-    setShowModal(!showModal);
+    setIsModalOpen((prevIsModalOpen) => !prevIsModalOpen);
   };
 
-  if (!showModal) {
+  const modalContent = (
     <div className="flex flex-col items-start gap-4 md:gap-6">
       <div className="border-b border-gray-200 pb-5">
         <div className="-ml-2 -mt-2 flex flex-wrap items-baseline">
@@ -36,9 +34,13 @@ const SubmitPost = ({ params, community }: SubmitPostProps) => {
             onClick={toggleModal}
             variant="subtle"
             className="h-6 w-6 p-0 rounded-md absolute top-4 left-4"
-            aria-label="close modal"
+            aria-label="toggle modal"
           >
-            <Minimize2 className="h-4 w-4 " />
+            {isModalOpen ? (
+              <Maximize2 className="h-4 w-4" />
+            ) : (
+              <Minimize2 className="h-4 w-4" />
+            )}
           </Button>
         </div>
       </div>
@@ -50,40 +52,15 @@ const SubmitPost = ({ params, community }: SubmitPostProps) => {
           Post
         </Button>
       </div>
-    </div>;
-  }
+    </div>
+  );
 
-  return (
-    <Modal modalContainer="max-w-2xl" showModal={showModal}>
-      <div className="flex flex-col items-start gap-4 md:gap-6">
-        <div className="border-b border-gray-200 pb-5">
-          <div className="-ml-2 -mt-2 flex flex-wrap items-baseline">
-            <h3 className="ml-2 mt-2 text-base font-semibold leading-6 text-gray-900">
-              Create Post
-            </h3>
-            <p className="ml-2 mt-1 truncate text-sm text-gray-500s">
-              in z/{params.slug}
-            </p>
-            <Button
-              onClick={toggleModal}
-              variant="subtle"
-              className="h-6 w-6 p-0 rounded-md absolute top-4 left-4"
-              aria-label="close modal"
-            >
-              <Maximize2 className="h-4 w-4 " />
-            </Button>
-          </div>
-        </div>
-
-        <Editor communityId={community.id} />
-
-        <div className="w-full flex justify-end">
-          <Button type="submit" className="w-full" form="community-post-form">
-            Post
-          </Button>
-        </div>
-      </div>
+  return isModalOpen ? (
+    <Modal modalContainer="max-w-2xl" showModal={false}>
+      {modalContent}
     </Modal>
+  ) : (
+    modalContent
   );
 };
 
