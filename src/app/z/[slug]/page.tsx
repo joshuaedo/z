86770df614faa7d834f9bb1587cea38f0eca0afe
Joshua Edo <joshua.edo01@gmail.com>
2023-08-31 12:20:12 +0000

    create Sidebar component

--- src/app/z/[slug]/page.tsx
+++ src/app/z/[slug]/page.tsx
@@ -3,9 +3,8 @@ import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { notFound } from "next/navigation";
-import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
-import { format } from "date-fns";
 import PostFeed from "@/components/PostFeed";
+import SideBar from "@/components/SideBar";
 
 interface SubmitPageProps {
     params: {
@@ -73,39 +72,7 @@ const SubmitPage = async ({ params }: SubmitPageProps) => {
           </div>
 
           {/* Info sidebar */}
-          <div className="col-span-1 overflow-hidden h-fit rounded-lg border border-gray-200 order-last">
-            <div className="px-6 py-4">
-              <p className="font-semi-bold py-3">About z/{community.name}</p>
-            </div>
-            <dl className="divide-y divide-gray-100 px-6 py-4 text-sm leading-6 bg-white">
-              <div className="flex justify-between gap-x-4 py-3">
-                <dt className="text-gray-500">Created</dt>
-                <dd className="text-gray-700">
-                  <time dateTime={community.createdAt.toDateString()}>
-                    {format(community.createdAt, "MMMM d, yyyy")}
-                  </time>
-                </dd>
-              </div>
-              <div className="flex justify-between gap-x-4 py-3">
-                <dt className="text-gray-500">Members</dt>
-                <dd className="text-gray-700">
-                  <div className="text-gray-900">{memberCount}</div>
-                </dd>
-              </div>
-              {community.creatorId === session?.user.id ? (
-                <div className="flex justify-between gap-x-4 py-3">
-                  <p className="text-gray-500">You created this community</p>
-                </div>
-              ) : null}
-              {community.creatorId !== session?.user.id ? (
-                <SubscribeLeaveToggle
-                  isSubscribed={isSubscribed}
-                  communityId={community.id}
-                  communityName={community.name}
-                />
-              ) : null}
-            </dl>
-          </div>
+          <SideBar community={community} session={session} isSubscribed={isSubscribed} memberCount={memberCount} />
 
         </div>
       </div>
