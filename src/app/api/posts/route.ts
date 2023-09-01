9b7c0f6c990097165e08af5841ba305916db7ec7
Joshua Edo <joshua.edo01@gmail.com>
2023-09-01 21:18:57 +0000

    Update route.ts

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -36,7 +36,51 @@ export async function GET(req: Request) {
         })
 
         let whereClause = {}
-    } catch (error) {
 
-    }
+        if (communityName) {
+            whereClause = {
+                community: {
+                    name: communityName,
+                },
+            }
+        } else if (session) {
+            whereClause = {
+                community: {
+                    id: {
+                        in: followedCommunitiesIds,
+                    }
+                }
+            }
+        }
+
+        const posts = await db.post.findMany({
+           take: parseInt(limit),
+           skip: (parseInt(page) - 1) * parseInt(limit),
+           orderBy: {
+            createdAt: "desc"
+           },
+           include: {
+            community: true,
+            votes: true,
+            author: true,
+            comments: true,
+           },
+           where: whereClause,
+        })
+
+        return new Response(JSON.stringify(posts))
+    }  catch (error) {
+        if (error instanceof z.ZodError) {
+          return new Response("Invalid request data" + error.message, {
+            status: 422,
+          });
+        }
+    
+        return new Response(
+          "Could not fetch more posts, please try again later",
+          {
+            status: 500,
+          }
+        );
+      }
 }
