3c40b95a0ebdf7ef18aafccc6fd49725091ed2bd
Joshua Edo <joshua.edo01@gmail.com>
2023-09-01 23:32:14 +0000

    Update PostVoteServer.tsx

--- src/components/post-vote/PostVoteServer.tsx
+++ src/components/post-vote/PostVoteServer.tsx
@@ -5,7 +5,7 @@ interface PostVoteServerProps {
   postId: string
   initialVotesAmt?: number
   initialVote?: VoteType | null
-  getData?: () => Promise<{Post & { votes: Vote[]}} | null>
+  getData?: () => Promise<(Post & { votes: Vote[]}) | null>
 }
 
 const PostVoteServer: FC<PostVoteServerProps> = ({ 
