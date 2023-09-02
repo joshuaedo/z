2f3c94a811473dba9ffb27ab4376f09029b3cda6
Joshua Edo <joshua.edo01@gmail.com>
2023-09-02 17:06:19 +0000

    Update Aside.tsx

--- src/components/layout/Aside.tsx
+++ src/components/layout/Aside.tsx
@@ -10,14 +10,29 @@ import { Home, UserCircle, Search, Users } from "lucide-react";
           <Icons.logo className="h-8 w-8 md:h-10 md:w-10" />
         </Link>
 
-        <Link href="/" className="flex items-center justify-center text-2xl font-medium p-2 rounded-lg">
-          <Home strokeWidth={1.2} className="h-7 w-7 md:h-9 md:w-9" />
+        <Link href="/" className="flex items-center justify-center text-2xl font-medium p-3 rounded-lg">
+           <Home strokeWidth={1.2} className="h-6 w-6 md:h-8 md:w-8 mr-2" />
            Home
         </Link>
 
-        <UserCircle strokeWidth={1.2} className="h-7 w-7 md:h-9 md:w-9" />
-        <Search strokeWidth={1.2} className="h-7 w-7 md:h-9 md:w-9" />
-        <Users strokeWidth={1.2} className="h-7 w-7 md:h-9 md:w-9" />
+        <Link href="/" className="flex items-center justify-center text-2xl font-medium p-3 rounded-lg">
+           <UserCircle strokeWidth={1.2} className="h-6 w-6 md:h-8 md:w-8 mr-2" />
+           Profile
+        </Link>
+
+        <Link href="/" className="flex items-center justify-center text-2xl font-medium p-3 rounded-lg">
+           <Search strokeWidth={1.2} className="h-6 w-6 md:h-8 md:w-8 mr-2" />
+           Explore
+        </Link>
+
+        <Link href="/" className="flex items-center justify-center text-2xl font-medium p-3 rounded-lg">
+           <Users strokeWidth={1.2} className="h-6 w-6 md:h-8 md:w-8 mr-2" />
+           Communities
+        </Link>
+
+        
+        
+        
         
 
       </aside>
