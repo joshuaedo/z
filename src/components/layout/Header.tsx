b4e44efd038400a60802fa6ea3828ee1f50a0f9c
Joshua Edo <joshua.edo01@gmail.com>
2023-09-02 17:16:35 +0000

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,8 +2,7 @@ import Link from "next/link";
 import { Icons } from "../Icons";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
-const Header = async () => {
-
+const Header = () => {
   return (
     <nav className="inline md:hidden fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-4">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-center relative">
@@ -17,8 +16,6 @@ const Header = async () => {
         </Link>
 
         {/* Search Bar */}
-
-
       </div>
     </nav>
   );
