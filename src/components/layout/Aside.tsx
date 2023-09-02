31591b1a3cdf4d6bb9d8a0f212fd078c71e2f17c
Joshua Edo <joshua.edo01@gmail.com>
2023-09-02 18:12:18 +0000

    Update layout.tsx and Aside.tsx

--- src/components/layout/Aside.tsx
+++ src/components/layout/Aside.tsx
@@ -12,27 +12,27 @@ import { buttonVariants } from "../ui/Button";
     return (
       <aside className="overflow-hidden h-fit rounded-lg border border-gray-200 fixed px-10 py-8 space-y-4">
         
-        <Link href="/" className="hidden md:flex items-center">
+        <Link href="/" className="hidden md:flex items-center px-2 ">
           <Icons.logo className="h-8 w-8 md:h-10 md:w-10" />
         </Link>
 
-        <Link href="/" className="flex items-end text-2xl font-medium py-1 pr-2 rounded-lg">
-           <Home strokeWidth={1.2} className="h-6 w-6 md:h-8 md:w-8 mr-2" />
+        <Link href="/" className="flex items-end text-xl font-medium py-1 px-2 rounded-lg">
+           <Home strokeWidth={1.5} className="h-5 w-5 md:h-7 md:w-7 mr-3" />
            Home
         </Link>
 
-        <Link href="/profile" className="flex items-end text-2xl font-medium py-1 pr-2 rounded-lg">
-           <UserCircle strokeWidth={1.2} className="h-6 w-6 md:h-8 md:w-8 mr-2" />
+        <Link href="/profile" className="flex items-end text-xl font-medium py-1 px-2  rounded-lg">
+           <UserCircle strokeWidth={1.5} className="h-5 w-5 md:h-7 md:w-7 mr-3" />
            Profile
         </Link>
 
-        <Link href="/explore" className="flex items-end text-2xl font-medium py-1 pr-2 rounded-lg">
-           <Search strokeWidth={1.2} className="h-6 w-6 md:h-8 md:w-8 mr-2" />
+        <Link href="/explore" className="flex items-end text-xl font-medium py-1 px-2  rounded-lg">
+           <Search strokeWidth={1.5} className="h-5 w-5 md:h-7 md:w-7 mr-3" />
            Explore
         </Link>
 
-        <Link href="/communities" className="flex items-end text-2xl font-medium py-1 pr-2 rounded-lg">
-           <Users strokeWidth={1.2} className="h-6 w-6 md:h-8 md:w-8 mr-2" />
+        <Link href="/communities" className="flex items-end text-xl font-medium py-1 px-2 rounded-lg">
+           <Users strokeWidth={1.5} className="h-5 w-5 md:h-7 md:w-7 mr-3" />
            Communities
         </Link>
 
@@ -49,7 +49,7 @@ import { buttonVariants } from "../ui/Button";
        <Link href="/privacy-policy" className="block text-xs font-medium rounded-lg">
            Privacy Policy
         </Link>
-        <Link href="/terms-of-service" className="block  font-medium rounded-lg">
+        <Link href="/terms-of-service" className="block text-xs font-medium rounded-lg">
            Terms of Service
         </Link>
         <Link href="/support" className="block text-xs font-medium rounded-lg">
