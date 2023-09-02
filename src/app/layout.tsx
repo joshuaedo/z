812d4e7e0aecd917085ee729c3adfbd9ba74b679
Joshua Edo <joshua.edo01@gmail.com>
2023-09-02 12:59:10 +0000

    Update layout.tsx and Aside.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,8 +63,8 @@ export default function RootLayout({
           {/* @ts-expect-error Server Component */}
           <Header />
           {modal}
-          <main className="container max-w-7xl mx-auto h-full pt-12">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
+          <main className="container max-w-5xl mx-auto h-full pt-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6 relative">
            <Aside />
             {children}
             </div>
--- src/components/layout/Aside.tsx
+++ src/components/layout/Aside.tsx
@@ -4,7 +4,7 @@ import { HomeIcon } from "@radix-ui/react-icons";
 
  const Aside = () => {
     return (
-      <aside className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
+      <aside className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first">
         <div className="bg-purple-100 px-6 py-4">
           <p className="font-semibold py-3 flex items-center gap-1.5">
            <HomeIcon className="w-4 h-4" />
