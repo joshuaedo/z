9acee25799a6a0294145927285fc4c9c5ff6210d
Joshua Edo <joshua.edo01@gmail.com>
2023-09-02 13:54:24 +0000

    Update Aside.tsx

--- src/components/layout/Aside.tsx
+++ src/components/layout/Aside.tsx
@@ -7,20 +7,20 @@ import { SearchIcon } from "lucide-react";
     return (
       <aside className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first">
         
-        <Link href="/" className="flex gap-2 items-center">
+        <Link href="/" className="hidden md:flex gap-2 items-center">
           <Icons.logo className="h-8 w-8 md:h-10 md:w-10" />
         </Link>
 
         <div className="px-6 py-4">
           <p className="font-semibold py-3 flex items-center gap-1.5">
-           <HomeIcon className="w-4 h-4" />
+           <HomeIcon className="h-7 w-7 md:h-9 md:w-9" />
             Home
           </p>
         </div>
 
-        <div className="divide-y divide-gray-100 px-6 py-4">
+        <div className="divide-y divide-gray-900 px-6 py-4">
         <p className="font-semibold py-3 flex items-center gap-1.5">
-         <SearchIcon className="w-4 h-4" />
+         <SearchIcon className="h-7 w-7 md:h-9 md:w-9" />
           Explore
           </p>
         </div>
