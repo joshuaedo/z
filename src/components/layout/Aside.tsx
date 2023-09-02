b5f5aa29563187131e7a34105583064ec5e0361a
Joshua Edo <joshua.edo01@gmail.com>
2023-09-02 16:18:05 +0000

    Update Aside.tsx

--- src/components/layout/Aside.tsx
+++ src/components/layout/Aside.tsx
@@ -1,25 +1,23 @@
 import Link from "next/link";
 import { Icons } from "../Icons";
-import { Home, UserCircle, Search, Users2 } from "lucide-react";
+import { Home, UserCircle, Search, Users } from "lucide-react";
 
  const Aside = () => {
     return (
-      <aside className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first">
+      <aside className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first fixed">
         
-        <Link href="/" className="hidden md:flex gap-2 items-center">
+        <Link href="/" className="hidden md:flex items-center">
           <Icons.logo className="h-8 w-8 md:h-10 md:w-10" />
         </Link>
 
-        <div className="px-6 py-4">
-          <p className="py-3 flex items-center gap-1.5">
-           <Home strokeWidth={1} className="h-7 w-7 md:h-9 md:w-9" />
-            <span className={`font-semibold`}>Home</span>
-          </p>
-        </div>
+        <Link href="/home" className="flex items-center justify-around">
+          <Home strokeWidth={1} className="h-7 w-7 md:h-9 md:w-9" />
+           Home
+        </Link>
 
         <UserCircle strokeWidth={1} className="h-7 w-7 md:h-9 md:w-9" />
         <Search strokeWidth={1} className="h-7 w-7 md:h-9 md:w-9" />
-        <Users2 strokeWidth={1} className="h-7 w-7 md:h-9 md:w-9" />
+        <Users strokeWidth={1} className="h-7 w-7 md:h-9 md:w-9" />
         
 
       </aside>
