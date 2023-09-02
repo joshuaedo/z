57d6f8301c7d283dfd70af7f5747e950e308625e
Joshua Edo <joshua.edo01@gmail.com>
2023-09-02 16:27:12 +0000

    Update Aside.tsx

--- src/components/layout/Aside.tsx
+++ src/components/layout/Aside.tsx
@@ -4,20 +4,20 @@ import { Home, UserCircle, Search, Users } from "lucide-react";
 
  const Aside = () => {
     return (
-      <aside className="overflow-hidden h-fit w-full rounded-lg border border-gray-200">
+      <aside className="overflow-hidden h-fit w-full rounded-lg border border-gray-200 fixed">
         
         <Link href="/" className="hidden md:flex items-center">
           <Icons.logo className="h-8 w-8 md:h-10 md:w-10" />
         </Link>
 
         <Link href="/home" className="flex items-center justify-around">
-          <Home strokeWidth={2} className="h-7 w-7 md:h-9 md:w-9" />
+          <Home strokeWidth={1.2} className="h-7 w-7 md:h-9 md:w-9" />
            Home
         </Link>
 
-        <UserCircle strokeWidth={2} className="h-7 w-7 md:h-9 md:w-9" />
-        <Search strokeWidth={2} className="h-7 w-7 md:h-9 md:w-9" />
-        <Users strokeWidth={2} className="h-7 w-7 md:h-9 md:w-9" />
+        <UserCircle strokeWidth={1.2} className="h-7 w-7 md:h-9 md:w-9" />
+        <Search strokeWidth={1.2} className="h-7 w-7 md:h-9 md:w-9" />
+        <Users strokeWidth={1.2} className="h-7 w-7 md:h-9 md:w-9" />
         
 
       </aside>
