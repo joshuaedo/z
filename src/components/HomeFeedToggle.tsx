fdc553f35c79442ab5cf2d2be23cdba0dab78b14
Joshua Edo <joshua.edo01@gmail.com>
2023-09-03 22:35:52 +0000

    Update HomeFeedToggle.tsx

--- src/components/HomeFeedToggle.tsx
+++ src/components/HomeFeedToggle.tsx
@@ -1,15 +1,19 @@
 "use client"
 
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
+
+{/* <a href="/" className={`py-2 px-3 hover:bg-[#F8FAFC] cursor-pointer mr-4 ${pathname === "/" ? "font-bold" : "opacity-60"}`}>For You</a>
+          <a href="/following" className={`py-2 px-3 hover:bg-[#F8FAFC] cursor-pointer ${pathname === "/following" ? "font-bold" : "opacity-60"}`}>Following</a> */}
 
 const HomeFeedToggle = () => {
+  const router = useRouter();
   const pathname = usePathname();
 
   return (
        <div className="flex w-full items-center justify-center">
          <div className='overflow-hidden max-w-4xl h-fit flex items-center justify-evenly rounded-lg shadow px-7 py-5 bg-white'>
-          <a href="/" className={`py-2 px-3 hover:bg-[#F8FAFC] cursor-pointer mr-4 ${pathname === "/" ? "font-bold" : "opacity-60"}`}>For You</a>
-          <a href="/following" className={`py-2 px-3 hover:bg-[#F8FAFC] cursor-pointer ${pathname === "/following" ? "font-bold" : "opacity-60"}`}>Following</a>
+          <button onClick={() => router.push('/')} className={`py-2 px-3 hover:bg-[#F8FAFC] cursor-pointer mr-4 ${pathname === "/" ? "font-bold" : "opacity-60"}`}>For You</button>
+          <button onClick={() => router.push('/following')} className={`py-2 px-3 hover:bg-[#F8FAFC] cursor-pointer ${pathname === "/following" ? "font-bold" : "opacity-60"}`}>Following</button>
         </div>
       </div>
   );
