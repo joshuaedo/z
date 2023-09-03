5f4ee520ff21127e25464d6065e8611dc36d66f7
Joshua Edo <joshua.edo01@gmail.com>
2023-09-03 22:14:27 +0000

    Update MobileNavClient.tsx

--- src/components/layout/MobileNavClient.tsx
+++ src/components/layout/MobileNavClient.tsx
@@ -11,21 +11,21 @@ const MobileNavClient = () => {
           href='/'
           className='flex items-end text-xl font-medium py-1 pr-2 rounded-lg'
         >
-          <Home strokeWidth={pathname === "/" ? 2.1 : 1.7} className='h-6 w-6' />
+          <Home strokeWidth={pathname === "/" ? 2.3 : 1.6} className='h-6 w-6' />
         </Link>
 
         <Link
           href='/communities'
-          className='flex items-end text-xl font-medium py-1 pr-2 rounded-lg'
+          className='flex items-end text-xl font-medium py-1 p-2 rounded-lg'
         >
-          <Users strokeWidth={pathname === "/communities" ? 2.1 : 1.7} className='h-6 w-6' />
+          <Users strokeWidth={pathname === "/communities" ? 2.3 : 1.6} className='h-6 w-6' />
         </Link>
 
         <Link
           href='/explore'
           className='flex items-end text-xl font-medium py-1 pr-2 rounded-lg'
         >
-          <Search strokeWidth={pathname === "/explore" ? 2.1 : 1.7} className='h-6 w-6' />
+          <Search strokeWidth={pathname === "/explore" ? 2.3 : 1.6} className='h-6 w-6' />
         </Link>
     </>
  )
