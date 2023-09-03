2c5ee4b5ca001b4aad872b6913b05e54beb49dfa
Joshua Edo <joshua.edo01@gmail.com>
2023-09-03 22:23:12 +0000

    Update HomeFeedToggle.tsx

--- src/components/HomeFeedToggle.tsx
+++ src/components/HomeFeedToggle.tsx
@@ -1,16 +1,15 @@
 "use client"
 
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 const HomeFeedToggle = () => {
-  const router = useRouter();
   const pathname = usePathname();
 
   return (
        <div className="flex w-full items-center justify-center">
          <div className='overflow-hidden max-w-4xl h-fit flex items-center justify-evenly rounded-lg shadow px-7 py-5 bg-white'>
-          <button onClick={() => router.push('/')} className={`py-2 px-3 hover:bg-[#F8FAFC] cursor-pointer mr-4 ${pathname === "/" ? "font-bold" : "opacity-60"}`}>For You</button>
-          <button onClick={() => router.push('/following')} className={`py-2 px-3 hover:bg-[#F8FAFC] cursor-pointer ${pathname === "/following" ? "font-bold" : "opacity-60"}`}>Following</button>
+          <a href="/" className={`py-2 px-3 hover:bg-[#F8FAFC] cursor-pointer mr-4 ${pathname === "/" ? "font-bold" : "opacity-60"}`}>For You</a>
+          <a href="/following" className={`py-2 px-3 hover:bg-[#F8FAFC] cursor-pointer ${pathname === "/following" ? "font-bold" : "opacity-60"}`}>Following</a>
         </div>
       </div>
   );
