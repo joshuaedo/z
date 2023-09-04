572af0b4f4598042b8980cffdae8d3fab1a4ec14
Joshua Edo <joshua.edo01@gmail.com>
2023-09-04 15:26:25 +0000

    Update 2 files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
+import ForYouFeed from "@/components/feeds/ForYouFeed";
 import GeneralFeed from "@/components/feeds/GeneralFeed";
 import HomeFeedToggle from "@/components/HomeFeedToggle";
+import { getAuthSession } from "@/lib/auth";
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const session = await getAuthSession()
+
   return (
-    <div className={`relative`}>
-      <HomeFeedToggle />
-      <div className={`pt-6`}>
-      {/* @ts-expect-error server-component */}
-        <GeneralFeed />
-      </div>
-    </div>
-  );
+      <div className={`relative`}>
+        { session ? 
+              (
+              <>
+              <HomeFeedToggle /> 
+              <div className={`pt-6`}> 
+              {/* @ts-expect-error Server Component */}           
+                    <ForYouFeedFeed />
+              </div>
+               </>
+               {/* @ts-expect-error Server Component */}
+               ) : <GeneralFeed />
+        }  
+     </div>
+  )
 }
---
+++ src/components/feeds/ForYouFeed.tsx
@@ -0,0 +1,48 @@
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/config';
+import { db } from '@/lib/db';
+import PostFeed from './PostFeed';
+import { getAuthSession } from '@/lib/auth';
+
+const ForYouFeed = async () => {
+  const session = await getAuthSession();
+
+  // Fetch the communities the user is following
+  const followedCommunities = await db.subscription.findMany({
+    where: {
+      userId: session?.user.id,
+    },
+    include: {
+      community: true,
+    },
+  });
+
+  // Get the IDs of communities the user is following
+  const followedCommunityIds = followedCommunities.map(({ community }) => community.id);
+
+  // Fetch posts from communities that the user is NOT following
+  const posts = await db.post.findMany({
+    where: {
+      NOT: {
+        community: {
+          name: {
+            in: followedCommunityIds,
+          },
+        },
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      votes: true,
+      author: true,
+      comments: true,
+      community: true,
+    },
+    take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+  });
+
+  return <PostFeed initialPosts={posts} />;
+};
+
+export default ForYouFeed;
