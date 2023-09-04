7f2999f6258d8011a2236818bae8e236477b85fa
Joshua Edo <joshua.edo01@gmail.com>
2023-09-04 15:42:37 +0000

    Update GeneralFeed.tsx

--- src/components/feeds/GeneralFeed.tsx
+++ src/components/feeds/GeneralFeed.tsx
@@ -3,20 +3,7 @@ import { db } from '@/lib/db';
 import PostFeed from './PostFeed';
 
 const GeneralFeed = async () => {
-  const posts = await db.post.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-    include: {
-      votes: true,
-      author: true,
-      comments: true,
-      community: true,
-    },
-    take: INFINITE_SCROLLING_PAGINATION_RESULTS,
-  });
-
-  return <PostFeed initialPosts={posts} />;
+ return <div>sup?</div>
 };
 
 export default GeneralFeed;
