d0acaf4806a716adcaf953962d44abda1f447895
Joshua Edo <joshua.edo01@gmail.com>
2023-09-04 14:53:30 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
-import GeneralFeed from "@/components/feeds/GeneralFeed";
+import PostFeed from "@/components/feeds/PostFeed";
 import HomeFeedToggle from "@/components/HomeFeedToggle";
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
 import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { ExtendedPost } from '@/types/db';
+import { FC } from "react";
 
-export default async function HomePage() {
+interface HomePageProps {
+  initialPosts: ExtendedPost[]; // Specify the type here
+}
+
+{/* @ts-expect-error Server Component */}
+const HomePage: FC<HomePageProps> = async ({ initialPosts }) => {
 
   const session = await getAuthSession()
 
@@ -10,9 +19,44 @@ export default async function HomePage() {
       <div className={`relative`}>
       { session && <HomeFeedToggle /> }
       <div className={`${session && "pt-6"}`}>
-               {/* @ts-expect-error server-component */}
-               <GeneralFeed />
+               
+        <PostFeed initialPosts={initialPosts} />
              </div> 
      </div>
   )
+}
+
+export default HomePage;
+
+
+export async function getServerSideProps() {
+  try {
+    // Fetch all posts from your database
+    const posts = await db.post.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        votes: true,
+        author: true,
+        comments: true,
+        community: true,
+      },
+      // You can adjust the take limit as needed
+      take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+    });
+
+    return {
+      props: {
+        initialPosts: posts,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: {
+        initialPosts: [],
+      },
+    };
+  }
 }
