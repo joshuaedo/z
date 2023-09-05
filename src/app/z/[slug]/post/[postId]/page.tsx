f6422d7f04bf84cd35d91082775f2b34252e47ce
Joshua <joshua.edo01@gmail.com>
2023-09-05 09:01:02 +0100

    create dynamic streaming logic

--- src/app/z/[slug]/post/[postId]/page.tsx
+++ src/app/z/[slug]/post/[postId]/page.tsx
@@ -1,46 +1,113 @@
-import { db } from "@/lib/db";
-import { redis } from "@/lib/redis";
-import { CachedPost } from "@/types/redis";
-import { Post, User, Vote } from "@prisma/client";
-import { notFound } from "next/navigation";
+import PostVoteServer from '@/components/post-vote/PostVoteServer';
+import { Button } from '@/components/ui/Button';
+import { db } from '@/lib/db';
+import { redis } from '@/lib/redis';
+import { cn } from '@/lib/utils';
+import { CachedPost } from '@/types/redis';
+import { Post, User, Vote } from '@prisma/client';
+import { Loader2 } from 'lucide-react';
+import { ArrowBigDown } from 'lucide-react';
+import { ArrowBigUp } from 'lucide-react';
+import { notFound } from 'next/navigation';
+import { Suspense } from 'react';
 
 interface PostPageProps {
-    params: {
-        postId: string
-    }
+  params: {
+    postId: string;
+  };
 }
 
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store"
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
 
-const PostPage = async ({params}: PostPageProps) => {
-    
-    const cachedPost = (await redis.hgetall(`post:${params.postId}`)) as CachedPost
+const PostPage = async ({ params }: PostPageProps) => {
+  const cachedPost = (await redis.hgetall(
+    `post:${params.postId}`
+  )) as CachedPost;
 
-    let post: (Post & {
+  let post:
+    | (Post & {
         votes: Vote[];
-        author: User
-      }) | null = null
+        author: User;
+      })
+    | null = null;
 
-      if (!cachedPost) {
-        post = await db.post.findFirst({
-            where:{
-              id: params.postId
-            },
-            include: {
-                author: true,
-                votes: true,
-            }
-        })
-      }
+  if (!cachedPost) {
+    post = await db.post.findFirst({
+      where: {
+        id: params.postId,
+      },
+      include: {
+        author: true,
+        votes: true,
+      },
+    });
+  }
 
-      if (!post && !cachedPost) return notFound()
-      
-    return (
-        <div>
-            <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">PostPage</div>
-        </div>   
-    )
+  if (!post && !cachedPost) return notFound();
+
+  return (
+    <div>
+      <div className='h-full flex flex-col sm:flex-row items-center sm:items-start justify-between'>
+        <Suspense fallback={<PostVoteShell />}>
+          {/* @ts-expect-error Server Component */}
+          <PostVoteServer
+            postId={post?.id ?? cachedPost.id}
+            getData={async () => {
+              return await db.post.findUnique({
+                where: {
+                  id: params.postId,
+                },
+                include: {
+                  votes: true,
+                },
+              });
+            }}
+          />
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+function PostVoteShell() {
+  return (
+    <div className='flex flex-col w-12 md:w-20 md:gap-4 md:pr-6 md:pb-4'>
+      <Button
+        size='sm'
+        variant='ghost'
+        aria-label='upvote'
+        className='hidden md:inline-flex'
+      >
+        <ArrowBigUp className={cn('h-4 w-4 md:h-5 md:w-5 text-zinc-700')} />
+      </Button>
+      <button
+        aria-label='upvote'
+        className='py-2 flex justify-center items-center md:hidden'
+      >
+        <ArrowBigUp className={cn('h-4 w-4 md:h-5 md:w-5 text-zinc-700')} />
+      </button>
+
+      <div className='text-center py-2 font-medium text-sm text-zinc-900'>
+        <Loader2 className='h-3 w-3 animate-spin' />
+      </div>
+
+      <Button
+        size='sm'
+        variant='ghost'
+        aria-label='downvote'
+        className='hidden md:inline-flex'
+      >
+        <ArrowBigDown className={cn('h-4 w-4 md:h-5 md:w-5 text-zinc-700')} />
+      </Button>
+      <button
+        aria-label='upvote'
+        className='py-2 flex justify-center items-center md:hidden'
+      >
+        <ArrowBigDown className={cn('h-4 w-4 md:h-5 md:w-5 text-zinc-700')} />
+      </button>
+    </div>
+  );
 }
 
 export default PostPage;
--- src/components/post-vote/PostVoteServer.tsx
+++ src/components/post-vote/PostVoteServer.tsx
@@ -1,22 +1,50 @@
-import { Post, Vote, VoteType } from "@prisma/client";
-import { FC } from "react";
+import { Post, Vote, VoteType } from '@prisma/client';
+import { getServerSession } from 'next-auth';
+import { notFound } from 'next/navigation';
+import PostVoteClient from './PostVoteClient';
 
 interface PostVoteServerProps {
-  postId: string
-  initialVotesAmt?: number
-  initialVote?: VoteType | null
-  getData?: () => Promise<(Post & { votes: Vote[]}) | null>
+  postId: string;
+  initialVotesAmt?: number;
+  initialVote?: VoteType | null;
+  getData?: () => Promise<(Post & { votes: Vote[] }) | null>;
 }
 
-const PostVoteServer: FC<PostVoteServerProps> = ({ 
-    postId, initialVote, initialVotesAmt, getData
- }) => {
- 
+const PostVoteServer = async ({
+  postId,
+  initialVote,
+  initialVotesAmt,
+  getData,
+}: PostVoteServerProps) => {
+  const session = await getServerSession();
+
+  let _votesAmt: number = 0;
+  let _currentVote: VoteType | null | undefined = undefined;
+
+  if (getData) {
+    const post = await getData();
+    if (!post) return notFound();
+
+    _votesAmt = post.votes.reduce((acc, vote) => {
+      if (vote.type === 'UP') return acc + 1;
+      if (vote.type === 'DOWN') return acc - 1;
+      else return acc;
+    }, 0);
+
+    _currentVote = post.votes.find(
+      (vote) => vote.userId === session?.user.id
+    )?.type;
+  } else {
+    _votesAmt = initialVotesAmt!;
+    _currentVote = initialVote;
+  }
 
   return (
-      <div className=''>
-          PostVoteServer
-      </div>
+    <PostVoteClient
+      postId={postId}
+      initialVotesAmt={_votesAmt}
+      initialVote={_currentVote}
+    />
   );
 };
 
