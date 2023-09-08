42498fedd4fd6550d0810e71a680a25afa455333
Joshua Edo <joshua.edo01@gmail.com>
2023-09-08 19:05:57 +0000

    tabnine is cool

--- src/app/z/[slug]/post/[postId]/page.tsx
+++ src/app/z/[slug]/post/[postId]/page.tsx
@@ -2,7 +2,7 @@ import PostVoteServer from '@/components/post-vote/PostVoteServer';
 import { Button } from '@/components/ui/Button';
 import { db } from '@/lib/db';
 import { redis } from '@/lib/redis';
-import { cn } from '@/lib/utils';
+import { cn, formatTimeToNow } from '@/lib/utils';
 import { CachedPost } from '@/types/redis';
 import { Post, User, Vote } from '@prisma/client';
 import { Loader2 } from 'lucide-react';
@@ -66,10 +66,14 @@ const PostPage = async ({ params }: PostPageProps) => {
           />
         </Suspense>
 
-        <div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
-           <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
-             Posted by u/{post?.author.username ?? cachedPost.authorUsername}
-           </p>
+        <div className='sm:w-0 w-full flex-1 bg-white p-4 rounded-sm'>
+          <p className='max-h-40 mt-1 truncate text-xs text-gray-500'>
+            Posted by u/{post?.author.username ?? cachedPost.authorUsername}{' '}
+            {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
+          </p>
+          <h1 className='text-xl font-semibold py-2 leading-6 text-gray-900'>
+            {post?.title ?? cachedPost.title}
+          </h1>
         </div>
       </div>
     </div>
