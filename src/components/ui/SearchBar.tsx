3dc79c1d814bd9fa9aca3b73153438ef8271b90b
Joshua Edo <joshua.edo01@gmail.com>
2023-09-10 19:51:31 +0000

    create route

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -1,14 +1,30 @@
 'use client';
 
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Command, CommandInput } from './Command';
 import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { Community, Prisma } from '@prisma/client';
 
 interface SearchBarProps {}
 
 const SearchBar: FC<SearchBarProps> = ({}) => {
-  const {} = useQuery({
-    queryFn: async () => {},
+  const [input, setInput] = useState<string>('');
+
+  const {
+    data: queryResults,
+    refetch,
+    isFetched,
+    isFetching,
+  } = useQuery({
+    queryFn: async () => {
+      if (!input) return [];
+
+      const { data } = await axios.get(`/api/search?q=${input}`);
+      return data as Community & {
+        _count: Prisma.CommunityCountOutputType;
+      };
+    },
     queryKey: ['search-query'],
     enabled: false,
   });
@@ -16,6 +32,10 @@ const SearchBar: FC<SearchBarProps> = ({}) => {
   return (
     <Command className='rounded-lg border max-w-lg z-50 overflow-visible h-fit'>
       <CommandInput
+        value={input}
+        onValueChange={(text) => {
+          setInput(text);
+        }}
         className='outline-none border-none focus focus:border-none focus:outline-none ring-0'
         placeholder='Explore'
       />
