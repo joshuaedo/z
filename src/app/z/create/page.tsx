4e07045c5e7d13b5f25138f60b69526d885a5345
edojoshua <144670998+edojoshua@users.noreply.github.com>
2023-09-13 11:57:26 +0000

    ...

--- src/app/z/create/page.tsx
+++ src/app/z/create/page.tsx
@@ -1,95 +1,12 @@
-"use client";
+import CreateCommunity from "@/components/community/CreateCommunity";
+import { getAuthSession } from "@/lib/auth";
 
-import { Button } from '@/components/ui/Button';
-import { Input } from '@/components/ui/Input';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
-import { useMutation } from "@tanstack/react-query";
-import axios, { AxiosError } from "axios";
-import { CreateCommunityPayload } from '@/lib/validators/community';
-import { toast } from '@/hooks/use-toast';
-import { useCustomToast } from '@/hooks/use-custom-toast';
+interface CreateCommunityPageProps {}
 
-const Create = ({ }) => {
-  const [input, setInput] = useState<string>('');
-  const router = useRouter();
-  const { loginToast } = useCustomToast();
+const CreateCommunityPage = async ({}: CreateCommunityPageProps) => {
+  const session = await getAuthSession();
 
-  const { mutate: createCommunity, isLoading } = useMutation({
-    mutationFn: async () => {
-      const payload: CreateCommunityPayload = {
-        name: input
-      };
-
-      const { data } = await axios.post("/api/community", payload);
-      return data as string;
-    },
-    onError: (err) => {
-      if (err instanceof AxiosError) {
-        if (err.response?.status === 409) {
-          return toast({
-            title: "Community already exists",
-            description: "Please choose a different community.",
-            variant: "destructive",
-          });
-        }
-
-        if (err.response?.status === 422) {
-          return toast({
-            title: "Invalid community name",
-            description: "Please choose a name between 3 and 21 characters.",
-            variant: "destructive",
-          });
-        }
-
-        if (err.response?.status === 401) {
-          return loginToast();
-        }
-      }
-
-      toast({
-        title: "Action failed",
-        description: "There was an error creating your community.",
-        variant: "destructive",
-      });
-    },
-
-    onSuccess: (data) => {
-      router.push(`/z/${data}`);
-    }
-  });
-
-  return (
-    <div className='container flex items-center h-full max-w-3xl mx-auto'>
-      <div className='relative bg-white w-full h-fit rounded-lg p-4 space-y-6'>
-        <div className='flex justify-between items-center'>
-          <h1 className='text-xl font-semibold'>Create a community</h1>
-
-          <hr className='bg-zinc-500 h-px' />
-
-        </div>
-
-        <div>
-          <p className="text-lg font-medium">Name</p>
-          <p className="text-xs pb-2">Community names including capitalization cannot be changed.</p>
-        </div>
-
-        <div className="relative">
-          <p className='absolute text-sm left-0 w-8 inset-y-0 grid place-items-center text-zinc-400'>z/</p>
-
-          <Input value={input} onChange={(e) => setInput(e.target.value)} className='pl-6' />
-        </div>
-
-        <div className='flex justify-end gap-4'>
-          <Button variant="subtle" onClick={() => router.back()}>Cancel</Button>
-          <Button isLoading={isLoading} disabled={input.length === 0}
-            onClick={() => createCommunity()}
-          >Create Community</Button>
-        </div>
-
-      </div>
-    </div>
-  );
+  return <CreateCommunity session={session} />;
 };
 
-export default Create;
+export default CreateCommunityPage;
---
+++ src/components/community/CreateCommunity.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React, { FC } from "react";
+
+import { Session } from "next-auth";
+import EditCommunityForm from "./EditCommunityForm";
+interface CreateCommunityProps {
+  session: Session | null;
+}
+
+const CreateCommunity: FC<CreateCommunityProps> = ({ session }) => {
+  return (
+    <div className="rounded-md bg-white shadow px-4 md:px-6 py-4 space-y-5">
+      <h2 className="text-xl font-semibold">Create Community</h2>
+      <hr />
+      <EditCommunityForm />
+    </div>
+  );
+};
+
+export default CreateCommunity;
