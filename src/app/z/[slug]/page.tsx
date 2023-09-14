5a44b73e6ed784aee3e53228daf39626d2d2e44e
edojoshua <144670998+edojoshua@users.noreply.github.com>
2023-09-14 16:58:48 +0000

    work

--- src/app/z/[slug]/page.tsx
+++ src/app/z/[slug]/page.tsx
@@ -71,11 +71,11 @@ const SlugPage = async ({ params }: SlugPageProps) => {
 
   return (
     <div className="space-y-6">
-      <div className="md:flex items-center justify-center gap-y-2 md:gap-x-2">
+      <div className="md:flex items-center gap-y-5 md:gap-x-2">
         <CommunityAvatar community={community} className="h-12 w-12" />
         <div className="flex w-fit items-center justify-center">
           {/* Community Name & Info */}
-          <h2 className="font-bold text-3xl md:text-4xl ml-2">
+          <h2 className="font-bold text-3xl md:text-4xl">
             z/{community.name}
           </h2>
           {/* Community Status */}
