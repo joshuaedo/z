ade82302e837f77497aee1132143a773cc07395f
edojoshua <144670998+edojoshua@users.noreply.github.com>
2023-09-18 01:13:46 +0000

    z

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,22 +1,22 @@
-import Link from 'next/link';
-import { Icons } from '../ui/Icons';
-import { HamburgerMenuIcon } from '@radix-ui/react-icons';
+import Link from "next/link";
+import { Icons } from "../ui/Icons";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from '../ui/DropDownMenu';
-import Aside from './Aside';
+} from "../ui/DropDownMenu";
+import Aside from "./Aside";
 
 const Header = () => {
   return (
-    <nav className='inline md:hidden fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-4'>
-      <div className='container max-w-7xl h-full mx-auto flex items-center justify-center relative'>
-        <div className='absolute top-2 left-8 bg-transparent'>
+    <nav className="inline md:hidden fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[60] py-4">
+      <div className="container max-w-7xl h-full mx-auto flex items-center justify-center relative">
+        <div className="absolute top-2 left-8 bg-transparent">
           <DropdownMenu>
             <DropdownMenuTrigger>
-              <HamburgerMenuIcon strokeWidth={1.7} className='h-6 w-6' />
+              <HamburgerMenuIcon strokeWidth={1.7} className="h-6 w-6" />
             </DropdownMenuTrigger>
             <DropdownMenuContent>
               <DropdownMenuItem>
@@ -27,8 +27,8 @@ const Header = () => {
           </DropdownMenu>
         </div>
 
-        <Link href='/' className='flex gap-2 items-center'>
-          <Icons.logo className='h-8 w-8' />
+        <Link href="/" className="flex gap-2 items-center">
+          <Icons.logo className="h-8 w-8" />
         </Link>
 
         {/* Search Bar */}
