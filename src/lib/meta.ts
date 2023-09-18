7573972c216ed1268c2bf003febe1f7dd289c619
Joshua Edo <joshua.edo01@gmail.com>
2023-09-18 05:10:57 +0100

    meta

---
+++ src/lib/meta.ts
@@ -0,0 +1,7 @@
+export const defaultTitle = 'Z';
+
+export const defaultDescription = 'The Z Network, Join the conversation.';
+
+const metaImage = 'https://joshuaedo.sirv.com/Z/Z.png';
+
+export { metaImage };
