7d62209c39fdc68224f0c1e846ba62bc054a6006
Joshua Edo <joshua.edo01@gmail.com>
2023-09-18 05:41:52 +0100

    increase post title

--- src/lib/validators/post.ts
+++ src/lib/validators/post.ts
@@ -1,10 +1,10 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const PostValidator = z.object({
   title: z
     .string()
-    .min(3, { message: "Title must be longer than 3 characters" })
-    .max(50, { message: "Title cannot be longer than 50 characters" }),
+    .min(3, { message: 'Title must be longer than 3 characters' })
+    .max(100, { message: 'Title cannot be longer than 100 characters' }),
   communityId: z.string(),
   content: z.any(),
 });
