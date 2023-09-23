8eb0c8b51b6cea21ed7438e64b9ef3e478e206fd
Tony <145384399+tonywonatony@users.noreply.github.com>
2023-09-23 08:27:35 +0000

    providers

--- src/components/wrappers/Providers.tsx
+++ src/components/wrappers/Providers.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { SessionProvider } from "next-auth/react";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
@@ -10,9 +10,9 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <SessionProvider>
-      {/* <NextThemesProvider attribute="class" defaultTheme="system" enableSystem> */}
+      <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-      {/* </NextThemesProvider> */}
+      </NextThemesProvider>
     </SessionProvider>
   );
 };
