822b12b7c4f7cafbf7f1129dadb7a43cf0af3447
Tony <145384399+tonywonatony@users.noreply.github.com>
2023-09-23 07:12:15 +0000

    subtle

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -14,6 +14,7 @@ const buttonVariants = cva(
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
           "border border-input hover:bg-accent hover:text-accent-foreground",
+        subtle: "hover:bg-zinc-200 bg-zinc-100 text-zinc-900",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
