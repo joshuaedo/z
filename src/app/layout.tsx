0e95dcc3f9722993b854cbe3b2c0d7dfeb00ff34
Tony <145384399+tonywonatony@users.noreply.github.com>
2023-09-23 07:50:04 +0000

    font-sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,39 @@
-import '@uploadthing/react/styles.css';
-import Aside from '@/components/layout/Aside';
-import Header from '@/components/layout/Header';
-import MobileNav from '@/components/layout/MobileNav';
-import Providers from '@/components/wrappers/Providers';
-import { Toaster } from '@/components/ui/Toaster';
-import { cn } from '@/lib/utils';
-import '@/styles/globals.css';
-import { Inter } from 'next/font/google';
-import { Metadata } from 'next';
+import "@uploadthing/react/styles.css";
+import Aside from "@/components/layout/Aside";
+import Header from "@/components/layout/Header";
+import MobileNav from "@/components/layout/MobileNav";
+import Providers from "@/components/wrappers/Providers";
+import { Toaster } from "@/components/ui/Toaster";
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { Metadata } from "next";
 
 export const generateMetadata = async ({}): Promise<Metadata> => {
   return {
-    title: 'Z',
+    title: "Z",
     description:
-      'Z, the network of diverse communities where you can explore your passions and interests. Join the conversation.',
+      "Z, the network of diverse communities where you can explore your passions and interests. Join the conversation.",
     openGraph: {
-      title: 'Z',
-      description: 'The Z Network, Join the conversation.',
-      url: 'https://z.joshuaedo.com',
-      siteName: 'Z',
+      title: "Z",
+      description: "The Z Network, Join the conversation.",
+      url: "https://z.joshuaedo.com",
+      siteName: "Z",
       images: [
         {
-          url: 'https://joshuaedo.sirv.com/Z/Z.png',
+          url: "https://joshuaedo.sirv.com/Z/Z.png",
           width: 200,
           height: 200,
         },
       ],
-      type: 'website',
+      type: "website",
     },
     twitter: {
-      card: 'summary',
-      title: 'Z',
-      description: 'The Z Network, Join the conversation.',
-      creator: 'Joshua Edo',
-      images: ['https://joshuaedo.sirv.com/Z/Z.png'],
+      card: "summary",
+      title: "Z",
+      description: "The Z Network, Join the conversation.",
+      creator: "Joshua Edo",
+      images: ["https://joshuaedo.sirv.com/Z/Z.png"],
     },
     robots: {
       follow: true,
@@ -46,7 +46,7 @@ export const generateMetadata = async ({}): Promise<Metadata> => {
   };
 };
 
-const inter = Inter({ subsets: ['latin'] });
+const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
 export default function RootLayout({
   children,
@@ -57,23 +57,23 @@ export default function RootLayout({
 }) {
   return (
     <html
-      lang='en'
+      lang="en"
       className={cn(
-        'bg-white text-slate-900 antialiased light',
-        inter.className
+        "bg-white text-slate-900 antialiased font-sans light",
+        fontSans.variable
       )}
     >
-      <body className='min-h-screen bg-slate-50 antialiased'>
+      <body className="min-h-screen bg-slate-50 antialiased">
         <Providers>
           <Header />
           {modal}
-          <main className='container max-w-5xl mx-auto h-full pt-24 pb-20 md:pt-8 md:pb-8'>
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 gap-x-4'>
-              <div className='hidden md:block col-span-1 h-full relative'>
+          <main className="container max-w-5xl mx-auto h-full pt-24 pb-20 md:pt-8 md:pb-8">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 gap-x-4">
+              <div className="hidden md:block col-span-1 h-full relative">
                 {/* @ts-expect-error Server Component */}
                 <Aside />
               </div>
-              <div className='col-span-1 md:col-span-2'>{children}</div>
+              <div className="col-span-1 md:col-span-2">{children}</div>
             </div>
           </main>
           <Toaster />
