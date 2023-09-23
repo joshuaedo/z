93918e38f6d01b66229e351a54df19c05518ef11
Tony <145384399+tonywonatony@users.noreply.github.com>
2023-09-23 11:26:14 +0000

    top-5

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
   return (
     <nav className="inline md:hidden fixed top-0 inset-x-0 h-[4rem] bg-zinc-100 dark:bg-[#000000] border-b border-zinc-300 dark:border-[#333333] z-[60]">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-center relative">
-        <div className="absolute top-4 left-8 bg-transparent">
+        <div className="absolute top-5 left-8 bg-transparent">
           <DropdownMenu>
             <DropdownMenuTrigger>
               <HamburgerMenuIcon strokeWidth={1.7} className="h-6 w-6" />
