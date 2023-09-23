a9118a8e35601736e04948ba9324e83b897bdf47
Tony <145384399+tonywonatony@users.noreply.github.com>
2023-09-23 18:47:51 +0000

    okayy

--- src/components/pages/Communities.tsx
+++ src/components/pages/Communities.tsx
@@ -35,50 +35,56 @@ const Communities: FC<CommunitiesProps> = ({ session, subs }) => {
           <span>Create a community</span>
         </button>
         <ul id="community" className=" space-y-3">
-          {subs.map((community) => (
-            <>
-              <hr />
-              <li
-                key={community.id}
-                className="py-1 flex items-center rounded-lg p-1"
-              >
-                <Link
-                  href={`z/${community.name}`}
-                  className="w-full h-full flex items-center gap-x-4 md:gap-x-5"
+          {subs ? (
+            subs.map((community) => (
+              <>
+                <hr />
+                <li
+                  key={community.id}
+                  className="py-1 flex items-center rounded-lg p-1"
                 >
-                  <CommunityAvatar
-                    community={community}
-                    className="h-12 w-12"
-                  />
-                  <div>
-                    <p className="hidden md:block font-medium">
-                      {`z/${
-                        community.name.length > 16
-                          ? community.name.slice(0, 15) + "..."
-                          : community.name
-                      }`}
-                    </p>
-                    <p className="md:hidden font-medium">
-                      {`z/${
-                        community.name.length > 11
-                          ? community.name.slice(0, 10) + "..."
-                          : community.name
-                      }`}
-                    </p>
+                  <Link
+                    href={`z/${community.name}`}
+                    className="w-full h-full flex items-center gap-x-4 md:gap-x-5"
+                  >
+                    <CommunityAvatar
+                      community={community}
+                      className="h-12 w-12"
+                    />
+                    <div>
+                      <p className="hidden md:block font-medium">
+                        {`z/${
+                          community.name.length > 16
+                            ? community.name.slice(0, 15) + "..."
+                            : community.name
+                        }`}
+                      </p>
+                      <p className="md:hidden font-medium">
+                        {`z/${
+                          community.name.length > 11
+                            ? community.name.slice(0, 10) + "..."
+                            : community.name
+                        }`}
+                      </p>
 
-                    <div className="flex items-center pt-2">
-                      <Users className="mr-2 h-4 w-4 opacity-70" />{" "}
-                      <span className="text-xs text-muted-foreground">
-                        <span>{`${community._count?.subscribers ?? 0} member${
-                          community._count?.subscribers === 1 ? "" : "s"
-                        }`}</span>
-                      </span>
+                      <div className="flex items-center pt-2">
+                        <Users className="mr-2 h-4 w-4 opacity-70" />{" "}
+                        <span className="text-xs text-muted-foreground">
+                          <span>{`${community._count?.subscribers ?? 0} member${
+                            community._count?.subscribers === 1 ? "" : "s"
+                          }`}</span>
+                        </span>
+                      </div>
                     </div>
-                  </div>
-                </Link>
-              </li>
-            </>
-          ))}
+                  </Link>
+                </li>
+              </>
+            ))
+          ) : (
+            <li className="w-full text-xs py-6 flex items-center justify-center">
+              <span>- communities you create or join show up here -</span>
+            </li>
+          )}
         </ul>
       </div>
     </main>
