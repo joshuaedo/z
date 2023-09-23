9beb7d00d5e4e0f146b2b375e10e0a3768907134
Tony <145384399+tonywonatony@users.noreply.github.com>
2023-09-23 19:07:15 +0000

    hmm

--- src/components/pages/Communities.tsx
+++ src/components/pages/Communities.tsx
@@ -35,58 +35,57 @@ const Communities: FC<CommunitiesProps> = ({ session, subs }) => {
           <span>Create a community</span>
         </button>
         <ul id="community" className=" space-y-3">
-          {subs.length !== 0 ? (
-            subs.map((community) => (
-              <>
-                <hr />
-                <li
-                  key={community.id}
-                  className="py-1 flex items-center rounded-lg p-1"
+          {subs.map((community) => (
+            <>
+              <hr />
+              <li
+                key={community.id}
+                className="py-1 flex items-center rounded-lg p-1"
+              >
+                <Link
+                  href={`z/${community.name}`}
+                  className="w-full h-full flex items-center gap-x-4 md:gap-x-5"
                 >
-                  <Link
-                    href={`z/${community.name}`}
-                    className="w-full h-full flex items-center gap-x-4 md:gap-x-5"
-                  >
-                    <CommunityAvatar
-                      community={community}
-                      className="h-12 w-12"
-                    />
-                    <div>
-                      <p className="hidden md:block font-medium">
-                        {`z/${
-                          community.name.length > 16
-                            ? community.name.slice(0, 15) + "..."
-                            : community.name
-                        }`}
-                      </p>
-                      <p className="md:hidden font-medium">
-                        {`z/${
-                          community.name.length > 11
-                            ? community.name.slice(0, 10) + "..."
-                            : community.name
-                        }`}
-                      </p>
+                  <CommunityAvatar
+                    community={community}
+                    className="h-12 w-12"
+                  />
+                  <div>
+                    <p className="hidden md:block font-medium">
+                      {`z/${
+                        community.name.length > 16
+                          ? community.name.slice(0, 15) + "..."
+                          : community.name
+                      }`}
+                    </p>
+                    <p className="md:hidden font-medium">
+                      {`z/${
+                        community.name.length > 11
+                          ? community.name.slice(0, 10) + "..."
+                          : community.name
+                      }`}
+                    </p>
 
-                      <div className="flex items-center pt-2">
-                        <Users className="mr-2 h-4 w-4 opacity-70" />{" "}
-                        <span className="text-xs text-muted-foreground">
-                          <span>{`${community._count?.subscribers ?? 0} member${
-                            community._count?.subscribers === 1 ? "" : "s"
-                          }`}</span>
-                        </span>
-                      </div>
+                    <div className="flex items-center pt-2">
+                      <Users className="mr-2 h-4 w-4 opacity-70" />{" "}
+                      <span className="text-xs text-muted-foreground">
+                        <span>{`${community._count?.subscribers ?? 0} member${
+                          community._count?.subscribers === 1 ? "" : "s"
+                        }`}</span>
+                      </span>
                     </div>
-                  </Link>
-                </li>
-              </>
-            ))
-          ) : (
-            <li className="w-full text-xs py-3 flex items-center justify-center">
-              <span>- communities you create/join show up here -</span>
-            </li>
-          )}
+                  </div>
+                </Link>
+              </li>
+            </>
+          ))}
         </ul>
       </div>
+      {subs.length < 3 && (
+        <li className="w-full text-xs py-6 flex items-center justify-center">
+          <span>- communities you create/join show up here -</span>
+        </li>
+      )}
     </main>
   );
 };
