6c1eb527e8adc5b1d0d73c58303abc6a797600a0
Joshua Edo <joshua.edo01@gmail.com>
2023-10-28 20:05:19 +0000

    add notis to routes

--- src/app/api/community/post/comment/route.ts
+++ src/app/api/community/post/comment/route.ts
@@ -1,39 +1,52 @@
-import { getAuthSession } from "@/lib/auth";
-import { db } from "@/lib/db";
-import { CommentValidator } from "@/lib/validators/comment";
-import { z } from "zod";
+import { getAuthSession } from '@/lib/auth';
+import { db } from '@/lib/db';
+import { CommentValidator } from '@/lib/validators/comment';
+import { z } from 'zod';
 
 export async function PATCH(req: Request) {
-    try {
-        const body = await req.json();
+  try {
+    const body = await req.json();
 
-        const { postId, text, replyToId } = CommentValidator.parse(body);
+    const { postId, text, replyToId } = CommentValidator.parse(body);
 
-        const session = await getAuthSession();
+    const session = await getAuthSession();
 
-        if (!session?.user) {
-            return new Response("Unauthorized", { status: 401 });
-        }
-
-        await db.comment.create({
-            data: {
-                text,
-                postId,
-                userId: session.user.id,
-                authorId: session.user.id,
-                replyToId,
-            }
-        });
-
-        return new Response("OK");
-
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            return new Response("Invalid request data" + error.message, {
-                status: 422,
-            });
-        }
-
-        return new Response("Could not reply to comment", { status: 500 });
+    if (!session?.user) {
+      return new Response('Unauthorized', { status: 401 });
     }
+
+    const newComment = await db.comment.create({
+      data: {
+        text,
+        postId,
+        userId: session.user.id,
+        authorId: session.user.id,
+        replyToId,
+      },
+    });
+
+    const notificationData = {
+      type: 'comment',
+      userId: session.user.id,
+      read: false,
+
+      postId,
+      commentId: newComment?.id,
+      commentText: text,
+    };
+
+    await db.notification.create({
+      data: notificationData,
+    });
+
+    return new Response('OK');
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response('Invalid request data' + error.message, {
+        status: 422,
+      });
+    }
+
+    return new Response('Could not create comment', { status: 500 });
+  }
 }
